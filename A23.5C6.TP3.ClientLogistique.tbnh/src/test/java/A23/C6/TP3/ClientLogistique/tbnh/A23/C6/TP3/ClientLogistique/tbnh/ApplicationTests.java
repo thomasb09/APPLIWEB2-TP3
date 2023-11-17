3a93d73124c3293e8b466b5e4705cbbde60f46fd
thomasb09 <bissont20@gmail.com>
2023-11-17 12:12:18 -0500

    first commit

---
+++ A23.5C6.TP3.ClientLogistique.tbnh/src/test/java/A23/C6/TP3/ClientLogistique/tbnh/A23/C6/TP3/ClientLogistique/tbnh/ApplicationTests.java
@@ -0,0 +1,13 @@
+package A23.C6.TP3.ClientLogistique.tbnh.A23.C6.TP3.ClientLogistique.tbnh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ A23.5C6.TP3.ServiceREST.tbnh/src/main/java/A23/C6/TP3/ServiceREST/tbnh/A23/C6/TP3/ServiceREST/tbnh/Application.java
@@ -0,0 +1,13 @@
+package A23.C6.TP3.ServiceREST.tbnh.A23.C6.TP3.ServiceREST.tbnh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
