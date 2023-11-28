a1f53058daba3cafd559992d337e31ee05c23071
thomasb09 <bissont20@gmail.com>
2023-11-28 12:21:36 -0500

    Something

--- A23.5C6.TP3.ClientLogistique.tbnh/src/main/java/A23/C6/TP3/ClientLogistique/tbnh/A23/C6/TP3/ClientLogistique/tbnh/Application.java
+++ A23.5C6.TP3.ClientLogistique.tbnh/src/main/java/A23/C6/TP3/ClientLogistique/tbnh/A23/C6/TP3/ClientLogistique/tbnh/Application.java
@@ -24,7 +24,7 @@ import java.util.List;
 @SpringBootApplication
 public class Application extends javafx.application.Application {
 
-	private static final String BASE_URL = "http://127.0.0.1:8080/";
+	private static final String BASE_URL = "http://172.20.45.21:8080/";
 	RestTemplate restTemplate = new RestTemplate();
 
 	@Override
